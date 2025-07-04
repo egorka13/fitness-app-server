aab408290b8c1416be4e75680d9bc8c1570038d5
egorka13 <shir-egor@yandex.ru>
2025-07-04 18:44:56 +0200

    add db background pinger

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import cors from '@fastify/cors';
 import dotenv from 'dotenv';
 
 import { exercisesRoutes } from './routes/exercises';
+import { publicRoutes } from './routes/public';
+import { db } from './db/knex';
 
 dotenv.config();
 
@@ -43,6 +45,24 @@ fastify.register(cors, {
 });
 
 fastify.register(exercisesRoutes);
+fastify.register(publicRoutes);
+
+// This function pings the database every 30 minutes to ensure the connection is alive.
+async function startDbPinger() {
+  const INTERVAL = 30 * 60 * 1000;
+
+  setInterval(async () => {
+    try {
+      await db.raw('SELECT 1');
+      console.log(`[DB Ping] ${new Date().toISOString()} - OK`);
+    } catch (err) {
+      console.error(
+        `[DB Ping] ${new Date().toISOString()} - ERROR:`,
+        (err as any)?.message || err
+      );
+    }
+  }, INTERVAL);
+}
 
 // Start server
 const start = async () => {
@@ -51,9 +71,12 @@ const start = async () => {
       port: Number(process.env.PORT) || 3000,
       host: '0.0.0.0',
     });
-    console.log(
-      `Server running at http://localhost:${process.env.PORT || 3000}`
-    );
+
+    const PORT = process.env.PORT || 3000;
+    const DOMAIN = process.env.DOMAIN || 'http://localhost';
+    console.log(`Server running at ${DOMAIN}:${PORT}`);
+
+    startDbPinger();
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
